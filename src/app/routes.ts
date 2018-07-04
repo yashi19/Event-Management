c3ea23ef3ed879f42dcbe8f28aa11c496784a707
yashi19 <yashikatanwar19@gmail.com>
2018-07-04 17:43:58 +0530

    userr1

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -12,16 +12,6 @@ export const appRoutes = [
     {path: 'events/:id',component:EventDetailsComponent,canActivate:[EventRouteActivator]},
     {path:'404',component:Error404Component},
     {path:'',redirectTo:'/events',pathMatch:'full'},
-    {path:'user',loadChildren:'./user/user.module#UserModule'},
-<<<<<<< HEAD
-   
-    {path:'profiles',loadChildren:'./user/user.module#UserModule'},
-<<<<<<< HEAD
-    {path:'users',loadChildren:'./user/user.module#UserModule'},
-=======
-    
->>>>>>> task2
-=======
->>>>>>> 075f77fdd9e4bd5094ff1a4baf6649029c01adbb
+ 
     
 ] 
