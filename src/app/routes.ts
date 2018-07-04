1feb83d3965c16c70ec0d931a955ecf8261814ae
yashi19 <yashikatanwar19@gmail.com>
2018-07-04 17:21:20 +0530

    changing from 1 user

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -12,9 +12,4 @@ export const appRoutes = [
     {path: 'events/:id',component:EventDetailsComponent,canActivate:[EventRouteActivator]},
     {path:'404',component:Error404Component},
     {path:'',redirectTo:'/events',pathMatch:'full'},
-    {path:'user',loadChildren:'./user/user.module#UserModule'},
-    {path:'user',loadChildren:'./user/user.module#UserModule'},
-    {path:'user',loadChildren:'./user/user.module#UserModule'},
-    {path:'user',loadChildren:'./user/user.module#UserModule'},
-    {path:'user',loadChildren:'./user/user.module#UserModule'},
-] 
+]
