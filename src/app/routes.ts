995d70b6e24776dab1df0820d0f87da18d3a4397
yashi19 <yashikatanwar19@gmail.com>
2018-07-04 16:32:18 +0530

    in dev using 1st user

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -13,5 +13,6 @@ export const appRoutes = [
     {path:'404',component:Error404Component},
     {path:'',redirectTo:'/events',pathMatch:'full'},
     {path:'user',loadChildren:'./user/user.module#UserModule'},
+    {path:'profiles',loadChildren:'./user/user.module#UserModule'},
     
 ] 
