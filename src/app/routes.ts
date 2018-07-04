39172a2eaee730be6993c472eed9b569cbec3b42
yashi19 <yashikatanwar19@gmail.com>
2018-07-04 17:22:25 +0530

    adding

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -15,6 +15,6 @@ export const appRoutes = [
     {path:'user',loadChildren:'./user/user.module#UserModule'},
    
     {path:'profiles',loadChildren:'./user/user.module#UserModule'},
-     {path:'users',loadChildren:'./user/user.module#UserModule'},
+    {path:'users',loadChildren:'./user/user.module#UserModule'},
     
 ] 
